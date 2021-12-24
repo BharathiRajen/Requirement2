5e2e68864b17b9ab97d43a1d681674b858427906
BharathiRajendran <84081813+BharathiRajenran@users.noreply.github.com>
2021-12-24 17:13:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requirement2
+{
+    class Program
+    {
+        public static Boolean ValidatePassword(String password)
+        {
+            int validConditions = 0;
+            foreach (char c in password)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            foreach (char c in password)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            if (validConditions == 0)
+                return false;
+            foreach (char c in password)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            if (validConditions == 1) return false;
+            if (validConditions == 2)
+            {
+                char[] special = { '@', '#', '$', '%', '^', '&', '+', '=' }; // or whatever
+                if (password.IndexOfAny(special) == -1) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the password to be validated:");
+            string password = Console.ReadLine(); if (ValidatePassword(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                Console.WriteLine("Password is not valid");
+            }
+        }
+    }
+}
